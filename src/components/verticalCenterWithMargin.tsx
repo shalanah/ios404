71a7bac73781b47ba2f4b27caa194362f6878a8c
shalanah <shalanahfaith@gmail.com>
2024-04-02 15:28:07 -0500

    refactor: share scale useSpring value for camera position change

--- src/components/verticalCenterWithMargin.tsx
+++ src/components/verticalCenterWithMargin.tsx
@@ -1,25 +1,22 @@
 import { Vector3 } from 'three';
-import React, { useEffect, useLayoutEffect, useRef } from 'react';
-import { useFrame, useThree } from '@react-three/fiber';
+import React, { useLayoutEffect } from 'react';
+import { useThree } from '@react-three/fiber';
 import { cartonHeight, cartonSide } from './milkcarton';
 import useCanIUseContext from '@/hooks/useCanIUseContext';
 import { a, SpringProps, useSpring } from '@react-spring/three';
 import { scaleOpts } from './links';
-import usePrevious from '../hooks/usePrevious';
 
 type Props = {
   children?: React.ReactNode;
-  scale?: number;
 };
 
 const getYPosition = (verticalView: boolean) => {
   const centerY = cartonHeight / 2;
-  const verticalOffset = verticalView && window.innerHeight < 600 ? 10 : 0; // visual centering top part of milk carton less important + recedes
-
+  // visual centering top part of milk carton less important + recedes
+  const verticalOffset = verticalView && window.innerHeight < 600 ? 10 : 0;
   return -centerY + verticalOffset;
 };
 
-// const config = { mass: 0.05, tension: 600, friction: 40 };
 const config = {
   mass: 0.25,
   tension: 800,
@@ -33,24 +30,34 @@ export const VerticalCenterWithMargin = ({ children = null }: Props) => {
   const { setPaginationHeight, paginationHeight, verticalView, scale } =
     useCanIUseContext();
   const { camera, size } = useThree();
-  const prevScale = usePrevious(scale);
 
   const y = getYPosition(verticalView);
   const position = [0, y, -cartonSide * (scale - 1)];
 
-  const [spring, api] = useSpring(() => ({
-    scale,
-    position,
-    config,
-  }));
-  useEffect(() => {
-    api.start({
+  const [spring] = useSpring(
+    () => ({
       scale,
       position,
       config,
-    });
-  }, [api, scale, position, config]);
+      onChange: {
+        scale: (v: number) => {
+          const cameraPos = camera.position;
+          const percent =
+            ((v - scaleOpts.min) * 1.5) / (scaleOpts.max - scaleOpts.min); // let's make it lerp faster 1.5x
+          const newPosition = startCameraPos
+            .clone()
+            .lerp(endCameraPos, Math.min(percent, 1));
+          if (!newPosition.equals(cameraPos)) {
+            camera.position.copy(newPosition);
+            camera.lookAt(0, 0, 0);
+          }
+        },
+      },
+    }),
+    [scale, position, config]
+  );
 
+  // Get pagination space - static from scale 1 (will update though with window resize)
   useLayoutEffect(() => {
     const cartonBottomPos = new Vector3(0, y, cartonSide);
     const mouseCoords = cartonBottomPos.project(camera); // help figure out how much pagination space we have
@@ -59,35 +66,6 @@ export const VerticalCenterWithMargin = ({ children = null }: Props) => {
       setPaginationHeight(margin);
     }
   }, [y, camera, size, paginationHeight, setPaginationHeight]);
-
-  const animatingCamera = useRef<null | number>(null);
-  useFrame(({ clock: { elapsedTime } }) => {
-    // TODO: Use useSpring here too somehow... look at lower level options to use same lerping
-    if (prevScale !== scale) {
-      if (animatingCamera.current === null) {
-        animatingCamera.current = elapsedTime;
-      }
-      const max = 0.3;
-      const delta = elapsedTime - animatingCamera.current;
-      const animationComplete = delta > max;
-      const cameraPos = camera.position;
-      const percent =
-        ((scale - scaleOpts.min) * 2) / (scaleOpts.max - scaleOpts.min); // let's make it lerp faster 2x
-      const newPosition = startCameraPos
-        .clone()
-        .lerp(endCameraPos, Math.min(percent, 1));
-      if (!newPosition.equals(cameraPos)) {
-        camera.position.copy(
-          cameraPos
-            .clone()
-            .lerp(newPosition, animationComplete ? 1 : delta / max)
-        );
-        camera.lookAt(0, 0, 0);
-      }
-    } else {
-      animatingCamera.current = null;
-    }
-  });
 
   return <a.group {...(spring as SpringProps)}>{children}</a.group>;
 };
